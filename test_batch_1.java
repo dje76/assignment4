4d5fbf9380816e85c9517cc87314aa15538ec8e3
dje76 <dje76@msstate.edu>
2016-05-03 22:59:39 -0500

    adding first test batch

---
+++ test_batch_1.java
@@ -0,0 +1,87 @@
+package assignment4;
+
+/**
+ *
+ * @author Daniel Eads dje76
+ * Tests the email verifier part of the application
+ */
+public class test_batch_1 {
+    public static void main(String[] args) {
+        functions function = new functions();
+        
+        String email1 = "dad@dad.com";
+        String email2 = "daddad.com";
+        String email3 = "dad@dadcom";
+        String email4 = "dad@dad.";
+        String email5 = "@.";
+        String email6 = "daddadcom";
+        String email7 = "@dad.com";
+        String email8 = "@dadcom";
+        String email9 = "@dad.commm";
+        
+        boolean expected1 = true;
+        boolean expected2 = false;
+        boolean expected3 = false;
+        boolean expected4 = false;
+        boolean expected5 = false;
+        boolean expected6 = false;
+        boolean expected7 = false;
+        boolean expected8 = false;  
+        boolean expected9 = false;
+        
+        if(expected1 == function.email_verifier(email1)){
+            System.out.println("Test 1 PASS");
+        }
+        else{
+            System.out.println("Test 1 FAIL");
+        }
+        if(expected2 == function.email_verifier(email2)){
+            System.out.println("Test 2 PASS");
+        }
+        else{
+            System.out.println("Test 2 FAIL");
+        }        
+        if(expected3 == function.email_verifier(email3)){
+            System.out.println("Test 3 PASS");
+        }
+        else{
+            System.out.println("Test 3 FAIL");
+        }        
+        if(expected4 == function.email_verifier(email4)){
+            System.out.println("Test 4 PASS");
+        }
+        else{
+            System.out.println("Test 4 FAIL");
+        }        
+        if(expected5 == function.email_verifier(email5)){
+            System.out.println("Test 5 PASS");
+        }
+        else{
+            System.out.println("Test 5 FAIL");
+        }        
+        if(expected6 == function.email_verifier(email6)){
+            System.out.println("Test 6 PASS");
+        }
+        else{
+            System.out.println("Test 6 FAIL");
+        }
+        if(expected7 == function.email_verifier(email7)){
+            System.out.println("Test 7 PASS");
+        }
+        else{
+            System.out.println("Test 7 FAIL");
+        }        
+        if(expected8 == function.email_verifier(email8)){
+            System.out.println("Test 8 PASS");
+        }
+        else{
+            System.out.println("Test 8 FAIL");
+        }     
+        if(expected9 == function.email_verifier(email9)){
+            System.out.println("Test 9 PASS");
+        }
+        else{
+            System.out.println("Test 9 FAIL");
+        }  
+    }
+}
